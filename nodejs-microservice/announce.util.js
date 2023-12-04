6e1c7c8739b334aef728c57d33490a180fffe7c0
Sayan Chakrabarti <sayanc@aggregateintelligence.com>
2023-12-04 20:54:09 +0530

    feat(nodejs microservice): added kafkajs lib function called announce & invoked it in hello route

---
+++ nodejs-microservice/announce.util.js
@@ -0,0 +1,54 @@
+const { Kafka } = require('kafkajs');
+const { v4: uuidv4 } = require('uuid');
+
+// Define the Kafka broker(s)
+const kafka = new Kafka({
+  clientId: 'my-producer',
+  brokers: ['redpanda:9092'], // Replace with your Redpanda broker address
+});
+
+// Create a Kafka producer
+const producer = kafka.producer();
+const admin = kafka.admin();
+
+// Function to produce a message
+const produceMessage = async (topic, msgBody) => {
+  // Example message to be published
+  const message = {
+    key: uuidv4(),
+    value: msgBody,
+  };
+  try {
+    // Connect to the admin client to manage topics
+    await admin.connect();
+
+    // Check if the topic exists, and create it if not
+    const topicExists = await admin.listTopics();
+    console.log("topicExists ==> ", topicExists)
+    if (!topicExists.includes(topic)) {
+      await admin.createTopics({
+        topics: [{ topic }],
+      });
+      console.log(`Topic '${topic}' created successfully!`);
+    }
+
+    // Connect to the producer
+    await producer.connect();
+
+    // Send the message to the specified topic
+    await producer.send({
+      topic,
+      messages: [message],
+    });
+
+    console.log('Message published successfully!');
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    // Disconnect the admin client and producer
+    await admin.disconnect();
+    await producer.disconnect();
+  }
+};
+
+module.exports = { announce: produceMessage }
